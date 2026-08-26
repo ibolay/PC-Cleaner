17c6b223c0382577d8c2390f8c026aa95402332c
ibolicyber <ibrahimcyber4@gmail.com>
2026-08-26 15:00:00 +0400

    Add basic Tkinter window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("PC Cleaner")
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("PC Cleaner")
+root.geometry("800x600")
+
+title = tk.Label(
+    root,
+    text="PC Cleaner",
+    font=("Segoe UI", 24, "bold")
+)
+
+title.pack(pady=30)
+
+root.mainloop()
